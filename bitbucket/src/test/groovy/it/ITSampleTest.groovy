f00466f838159c2b0eef313a844f1e3e000c1d74
Roland Holban <rholban@adaptavist.com>
2019-10-23 13:24:18 -0500

    fix Jira Software and Service Desk versions

---
+++ bitbucket/src/test/groovy/it/ITSampleTest.groovy
@@ -0,0 +1,10 @@
+package it
+
+import spock.lang.Specification
+
+class ITSampleTest extends Specification {
+    def "test sample"() {
+        expect:
+        true
+    }
+}
---
+++ jira/src/test/groovy/it/ITSampleTest.groovy
@@ -0,0 +1,11 @@
+package it
+
+import com.atlassian.jira.component.ComponentAccessor
+import spock.lang.Specification
+
+class ITSampleTest extends Specification {
+    def "x"() {
+        expect:
+        true
+    }
+}
